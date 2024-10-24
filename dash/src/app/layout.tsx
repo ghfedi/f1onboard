0cf148de6655c0066eb9edd5a5f1e06426c6b2a6
Fedi Ghribi <ghribi.fadi@hotmail.fr>
2024-10-24 22:50:18 +0100

    edit docker compose

--- dash/src/app/layout.tsx
+++ dash/src/app/layout.tsx
@@ -12,20 +12,12 @@ export { metadata } from "@/metadata";
 export { viewport } from "@/viewport";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-	const enableTracking = !!env.NEXT_PUBLIC_ENABLE_TRACKING;
 
 	return (
 		<html lang="en" className={`${GeistSans.variable} ${GeistMono.variable} bg-zinc-950 font-sans text-white`}>
 			<head />
 
-			{enableTracking && (
-				<Script
-					async
-					defer
-					data-website-id="f1f0eb93-0656-4791-900d-b9a1b0e7af96"
-					src="https://base.slowly.dev/rep.js"
-				/>
-			)}
+
 
 			<body>{children}</body>
 		</html>
